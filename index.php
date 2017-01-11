65f57497b293fb654056b4b44a43dc1f98802c68
Hugi Romane <romane.h@codeur.online>
2017-01-11 10:42:51 +0100

    Affichage des dossiers et fichiers à l'intérieur

--- index.php
+++ index.php
@@ -31,27 +31,28 @@
 			<body>
 				<h1>Files Explorer</h1>
 
-				<?php foreach($dirs as $dir):?>
-					<?php if (is_dir($base_url.$dir)){?>
-						<?php if ($dir == "..") {?>
+				foreach($dirs as $dir)
+					if (is_dir($base_url.$dir))
+					{
+						 if ($dir == "..") {
 							<div>
 								<a href="index.php"><?=$dir?></a><br>
 							</div>
-						<?php } else {?>
+						 } else {
 							<div>
-								<?php if (isset($_GET['dossier'])) {?>
-									<a href="index.php?dossier=<?=$_GET['dossier']?><?=$dir?>/"><?=$dir?></a><br>
-								<?php } else {?>
+								 if (isset($_GET['dossier'])) {
+									<a href="index.php?dossier=<?=$_GET['dossier']?><?=$dir?>/"><?=$dir</a><br>
+								 } else {
 									<a href="index.php?dossier=<?=$dir?>/"><?=$dir?></a><br>
-								<?php }?>
+								 }
 							</div>
-						<?php }?>
-					<?php } else {?>
+						 }
+					 } else {
 						<div>
-							<p><?=$dir;?></p>
+							<p>$dir;</p>
 						</div>
-					<?php }?>
-				<?php endforeach;?>
+					 }
+				 endforeach;
 			</body>
 
 
